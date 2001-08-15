e232d4464fcf1f0039824bd00adb02cf49be1c65
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2001-08-15 16:37:17 +0000

    *** empty log message ***

---
+++ benchmark/bench.sh
@@ -0,0 +1,15 @@
+#/bin/csh -f
+
+cd ../src
+set refin=../benchmark/benchmark1.inp
+set refout=../benchmark/benchmark.out
+
+#if ($#argv == 0 ) then
+#   echo "./bencht.sh ?"
+#   exit
+#endif
+
+./gridsetup.py 1 1 1 64 64 64
+make >& /dev/null ;   ./dns < $refin 
+
+
