516fb7245a4f6a321cd7ee62cf215f31fa607ebc
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2001-09-26 00:32:07 +0000

    random initial data work

--- src/init_data.F90
+++ src/init_data.F90
@@ -1,36 +1,130 @@
 
 
-subroutine init_data_test(Q)
+subroutine init_data_lwisotropic(Q)
+!
+! low wave number, quasi isotropic initial condition
+!
 use params
 implicit none
 real*8 :: Q(nx,ny,nz,n_var)
 real*8 :: work(nx,ny,nz)
-integer km,jm,im,i,j,k,n
-real*8 xw,R,theta
+integer km,jm,im,i,j,k,n,wn
+real*8 xw,ener_target,ener,xfac
+
+type wnforcing_d
+   real*8  :: wn
+   integer :: n
+   integer, allocatable :: index(:,:)
+end type
+type(wnforcing_d) :: wnforcing(2)
 
 Q=0
-do n=1,3
-call fft3d(Q(1,1,1,n),work)
 
-   do k=nz1,nz2
-      km=kmcord(k)
-      do j=ny1,ny2
-         jm=jmcord(j)
-         do i=nx1,nx2
-            im=imcord(i)
-            xw=sqrt(real(km**2+jm**2+im**2))
-            if (xw>0 .and. xw<=2.5) then
-               R=xw**(-5/3.0)
-               call random_number(theta)
-               theta=pi2*theta
-               !  a_lmn 
-            endif
-         enddo
+do n=1,2
+   wnforcing(n)%wn=n
+   wnforcing(n)%n=0
+enddo
+
+! count the number of wavenumbers in each band.  
+do k=nz1,nz2
+   km=kmcord(k)
+   do j=ny1,ny2
+      jm=jmcord(j)
+      do i=nx1,nx2
+         im=imcord(i)
+         xw=sqrt(real(km**2+jm**2+im**2))
+         if (xw>=.5 .and. xw<1.5) then
+            wnforcing(1)%n=wnforcing(1)%n+1
+         endif
+         if (xw>=1.5 .and. xw<2.5) then
+            wnforcing(2)%n=wnforcing(2)%n+1
+         endif
       enddo
    enddo
-
-call ifft3d(Q(1,1,1,n),work)
 enddo
+
+! allocate storage
+do n=1,2
+   i=wnforcing(n)%n
+   allocate(wnforcing(n)%index(i,3))
+   print *,'wave no=',wnforcing(n)%wn,' number of coeffs=',wnforcing(n)%n
+   wnforcing(n)%n=0  ! reset counter to use again below
+enddo
+
+! store all the indexes
+do k=nz1,nz2
+   km=kmcord(k)
+   do j=ny1,ny2
+      jm=jmcord(j)
+      do i=nx1,nx2
+         im=imcord(i)
+         xw=sqrt(real(km**2+jm**2+im**2))
+         if (xw>=.5 .and. xw<1.5) then
+            wnforcing(1)%n=wnforcing(1)%n+1
+            wnforcing(1)%index(wnforcing(1)%n,1)=i
+            wnforcing(1)%index(wnforcing(1)%n,2)=j
+            wnforcing(1)%index(wnforcing(1)%n,3)=k
+         endif
+
+         if (xw>=1.5 .and. xw<2.5) then
+            wnforcing(2)%n=wnforcing(2)%n+1
+            wnforcing(2)%index(wnforcing(2)%n,1)=i
+            wnforcing(2)%index(wnforcing(2)%n,2)=j
+            wnforcing(2)%index(wnforcing(2)%n,3)=k
+         endif
+      enddo
+   enddo
+enddo
+
+
+do wn=1,2
+   ener_target=(wnforcing(wn)%wn)**(-5.0/3.0)
+   do 
+      !random initial condition
+      !pick a random point in the n dimensional sphere
+      ! 1. first pick a randome point in R^n
+      ! 2. through away points outside the sphere.  
+      ! 3. When we get a point inside, project onto the surface.    
+      ener=0
+      do n=1,6 !wnforcing(wn)%n
+         i=wnforcing(1)%index(n,1)
+         j=wnforcing(1)%index(n,2)
+         k=wnforcing(1)%index(n,3)
+         km=kmcord(k)
+         jm=jmcord(j)
+         im=imcord(i)
+         call random_number(Q(i,j,k,1))
+         call random_number(Q(i,j,k,2))
+         call random_number(Q(i,j,k,3))
+         Q(i,j,k,:)=2*Q(i,j,k,:)-1
+
+         xfac = 2*2*2*(g_nx*g_ny*g_nz)
+         xfac=1
+!         if (km==0) xfac=xfac/2
+!         if (jm==0) xfac=xfac/2
+!         if (im==0) xfac=xfac/2
+         ener=ener + xfac*(Q(i,j,k,1)**2 + Q(i,j,k,2)**2 + Q(i,j,k,3)**2)
+      enddo
+!MPI SUM
+      ener=sqrt(ener)
+      print *,ener,wnforcing(wn)%n
+      if (ener<=1 .and. ener>.000001) exit
+   enddo
+   stop
+   ! normalize
+   do n=1,wnforcing(wn)%n
+      i=wnforcing(1)%index(n,1)
+      j=wnforcing(1)%index(n,2)
+      k=wnforcing(1)%index(n,3)
+      Q(i,j,k,:)=Q(i,j,k,:)*sqrt(ener_target/ener)
+   enddo
+
+enddo
+
+do n=1,3
+   call ifft3d(Q(1,1,1,n),work)  ! use rhs as a work array
+enddo
+
 
 end subroutine
 
