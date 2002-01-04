1fa4915d0fee0ddcea7d735afd30cf8748a8bd15
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-01-04 19:54:01 +0000

    *** empty log message ***

---
+++ testing/testg.sh
@@ -0,0 +1,55 @@
+#/bin/csh -f
+
+cd ../src
+set refin=../testing/ghost2d.inp
+set refout=../testing/ghost2d.out
+set tmp=/tmp/temp.out
+
+if ($#argv == 0 ) then
+   echo "./test.sh [1,p,makeref]"
+   echo " 1 = run 1 simple 2D test case"
+   echo " p = run some parallel cases"
+   echo " makeref  = generate new reference output, 2D"
+   exit
+endif
+
+if ($1 == makeref) then
+
+   ./gridsetup.py 1 1 1 128 128 1 
+   make dnsgrid; rm -f $refout 
+   ./dnsgrid < $refin > $refout
+  cat $refout
+
+endif
+
+if ($1 == 1) then
+
+./gridsetup.py 1 1 1 128 128 1  2 2 0 2 2 0
+make dnsgrid >& /dev/null ;  rm -f $tmp ; ./dnsgrid < $refin > $tmp 
+../testing/check.sh $tmp $refout
+
+./gridsetup.py 1 1 1 128 128 1  2 2 0 2 2 0
+make dnsghost >& /dev/null ;  rm -f $tmp ; ./dnsghost < $refin > $tmp 
+../testing/check.sh $tmp $refout
+
+
+endif
+
+if ($1 == p) then
+
+
+./gridsetup.py 2 1 1 128 128 1  2 2 0 2 2 0
+make dnsghost >& /dev/null ;  rm -f $tmp ; mpirun -np 2 ./dnsghost < $refin > $tmp 
+../testing/check.sh $tmp $refout
+
+./gridsetup.py 1 2 1 128 128 1  2 2 0 2 2 0
+make dnsghost >& /dev/null ;  rm -f $tmp ; mpirun -np 2 ./dnsghost < $refin > $tmp 
+../testing/check.sh $tmp $refout
+
+./gridsetup.py 2 2 1 128 128 1  2 2 0 2 2 0
+make dnsghost >& /dev/null ;  rm -f $tmp ; mpirun -np 4 ./dnsghost < $refin > $tmp 
+../testing/check.sh $tmp $refout
+
+
+endif
+
