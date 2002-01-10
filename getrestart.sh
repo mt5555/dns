fa9ee0157978aa8199bf8ab05cc43b093a558c59
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-01-10 17:22:54 +0000

    *** empty log message ***

---
+++ getrestart.sh
@@ -0,0 +1,108 @@
+#! /bin/csh 
+#
+# get some restart files from HPSS or specified directory
+# using the specified basename, find the file of the form basename????.????.u
+# with the newest time stamp (????.????)
+#
+#  $1 = basename of restart file. 
+#  $2 = HPSS   get file off of HPSS
+#     = path   get file from specified path
+#
+# status=0  success
+# status=1  failed
+#
+set name = $1
+set path = $2
+
+if ($path == HPSS) then
+
+   #search HPSS for newest restart file
+   set resnameu = `psi ls dns/{$name}\*.u | sort | tail -1`
+   if ($resnameu =="") then
+      echo "Error finding restart file.  Exit"
+      exit 1
+   else
+      set resnamev = `psi ls  dns/{$name}\*.v | sort | tail -1`
+      set resnamew = `psi ls  dns/{$name}\*.w | sort | tail -1`
+      echo "Using restart files: " 
+      echo $resnameu
+      echo $resnamev
+      echo $resnamew
+   endif
+   set resnameu2 = `basename $resnameu`
+   set resnamev2 = `basename $resnamev`
+   set resnamew2 = `basename $resnamew`
+
+   # check to see if files are left over from last run: 
+   if !(-e $resnameu2) then
+      psi get $resnameu
+   endif
+   if !(-e $resnamev2) then
+      psi get $resnamev
+   endif
+   if !(-e $resnamew2) then
+      psi get $resnamew
+   endif
+
+   \rm -f restart.*
+   \mv -f $resnameu2  restart.u
+   \mv -f $resnamev2  restart.v
+   \mv -f $resnamew2  restart.w
+   if !(-e restart.w) then
+      echo "No restart.w file"
+      exit 1
+   endif
+
+else
+
+   #search $path for newest restart file
+   set resnameu = `\ls {$path}/{$name}\*.u | sort | tail -1`
+   if ($resnameu =="") then
+      echo "Error finding restart file.  Exit"
+      exit 1
+   else
+      set resnamev = `ls  {$path}/{$name}\*.v | sort | tail -1`
+      set resnamew = `ls  {$path}/{$name}\*.w | sort | tail -1`
+      echo "Using restart files: " 
+      echo $resnameu
+      echo $resnamev
+      echo $resnamew
+   endif
+   set resnameu2 = `basename $resnameu`
+   set resnamev2 = `basename $resnamev`
+   set resnamew2 = `basename $resnamew`
+
+   # check to see if files are left over from last run: 
+   if !(-e $resnameu2) then
+      \cp $resnameu .
+   endif
+   if !(-e $resnamev2) then
+      \cp $resnamev .
+   endif
+   if !(-e $resnamew2) then
+      \cp $resnamew .
+   endif
+
+   \rm -f restart.*
+   \mv -f $resnameu2  restart.u
+   \mv -f $resnamev2  restart.v
+   \mv -f $resnamew2  restart.w
+   if !(-e restart.w) then
+      echo "No restart.w file"
+      exit 1
+   endif
+
+endif
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
