7e724f91babddf6231111f7e8628a7965116f8f8
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-07-15 17:01:29 +0000

    added cubic splines and averaging to isoave.m

--- matlab/isoave.m
+++ matlab/isoave.m
@@ -22,21 +22,40 @@ if (ntran==2)
    D2_ltt=fread(fid,[ndelta,ndir],'float64');
 end
 
+xx=1:.25:32;
+yyave=0*xx;
 
 figure(1)
 for i=1:ndir
-  semilogx( r_val(:,i),D_ll(:,i) )
+  x=r_val(:,i); 
+  y=D_ll(:,i);
+  semilogx( x,y,'.' )
   hold on;
-    semilogx( r_val(:,i),D_ll(:,i),'o' )
+  yy=spline(x,y,xx);
+  %semilogx( xx,yy);
+  yyave=yyave+yy;
 end
+yyave=yyave/ndir;
+semilogx( xx,yyave,'r');
+%axis([1 64 0 4]);
 hold off;
 
+
 figure(2)
+yyave=0*xx;
 for i=1:ndir
-  semilogx( r_val(:,i),D_lll(:,i) )
+  x=r_val(:,i); 
+  y=D_lll(:,i)./(x.^3);
+  semilogx( x,y,'.' )
   hold on;
-    semilogx( r_val(:,i),D_lll(:,i),'o' )
+  yy=spline(x,y,xx);
+  %semilogx( xx,yy);
+  yyave=yyave+yy;
 end
+yyave=yyave/ndir;
+semilogx( xx,yyave,'r');
+%axis([1 64 -.01 .01]);
 hold off;
+
 
 
