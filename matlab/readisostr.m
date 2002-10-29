a31a3b87562adc9fc4570ed1e814ee0f904762b2
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-10-29 22:00:08 +0000

    *** empty log message ***

---
+++ matlab/readisostr.m
@@ -0,0 +1,115 @@
+function ...
+    [nx,ndelta,ndir,r_val,ke,epsilon,mu,...
+     D_ll,D_lll,SP_lll,SN_lll,D1_tt,D2_tt,D1_ltt,D2_ltt,...
+     SP1_ltt,SP2_ltt,SN1_ltt,SN2_ltt] ...
+     = readisostr(fname)
+
+
+fid=fopen(fname,'r','b');
+
+ndelta=fread(fid,1,'float64');
+ndir  =fread(fid,1,'float64');
+nlon  =fread(fid,1,'float64');
+ntran =fread(fid,1,'float64');
+nscalars =fread(fid,1,'float64');
+nnew2 =fread(fid,1,'float64');
+
+
+r_val=fread(fid,[ndelta,ndir],'float64');
+if (nlon==2) 
+   D_ll=fread(fid,[ndelta,ndir],'float64');
+   D_lll=fread(fid,[ndelta,ndir],'float64');
+end
+if (nlon==4) 
+   D_ll=fread(fid,[ndelta,ndir],'float64');
+   D_lll=fread(fid,[ndelta,ndir],'float64');
+   SP_lll=fread(fid,[ndelta,ndir],'float64');
+   SN_lll=fread(fid,[ndelta,ndir],'float64');
+end
+if (nlon==11) 
+   Dl=zeros([ndelta,ndir,9]);
+   for p=2:10 
+     temp=fread(fid,[ndelta,ndir],'float64');
+     Dl(:,:,p-1)=temp;
+     if (p==2) D_ll=temp; end;
+     if (p==3) D_lll=temp; end;
+   end
+   SP_lll=fread(fid,[ndelta,ndir],'float64');
+   SN_lll=fread(fid,[ndelta,ndir],'float64');
+end
+if (nlon==12) 
+   Dl=zeros([ndelta,ndir,9]);
+   for p=2:10 
+     temp=fread(fid,[ndelta,ndir],'float64');
+     Dl(:,:,p-1)=temp;
+     if (p==2) D_ll=temp; end;
+     if (p==3) D_lll=temp; end;
+   end
+   SP_lll=fread(fid,[ndelta,ndir],'float64');
+   SN_lll=fread(fid,[ndelta,ndir],'float64');
+   H_ltt=fread(fid,[ndelta,ndir],'float64');
+end
+
+if (ntran==2) 
+   D1_tt=fread(fid,[ndelta,ndir],'float64');
+   D2_tt=fread(fid,[ndelta,ndir],'float64');
+   D1_ltt=fread(fid,[ndelta,ndir],'float64');
+   D2_ltt=fread(fid,[ndelta,ndir],'float64');
+end
+if (ntran==4) 
+   D1_tt=fread(fid,[ndelta,ndir],'float64');
+   D2_tt=fread(fid,[ndelta,ndir],'float64');
+   D1_ltt=fread(fid,[ndelta,ndir],'float64');
+   D2_ltt=fread(fid,[ndelta,ndir],'float64');
+   SP1_ltt=fread(fid,[ndelta,ndir],'float64');
+   SP2_ltt=fread(fid,[ndelta,ndir],'float64');
+   SN1_ltt=fread(fid,[ndelta,ndir],'float64');
+   SN2_ltt=fread(fid,[ndelta,ndir],'float64');
+end
+if (ntran==12) 
+  Dt1=zeros([ndelta,ndir,9]);
+  Dt2=zeros([ndelta,ndir,9]);
+  for p=2:10
+   temp1=fread(fid,[ndelta,ndir],'float64');    
+   temp2=fread(fid,[ndelta,ndir],'float64');    
+   Dt1(:,:,p-1)=temp1;
+   Dt2(:,:,p-1)=temp2;
+   if (p==2)
+     D1_tt=temp1;
+     D2_tt=temp2;
+   end
+ end 
+   D1_ltt=fread(fid,[ndelta,ndir],'float64');
+   D2_ltt=fread(fid,[ndelta,ndir],'float64');
+   SP1_ltt=fread(fid,[ndelta,ndir],'float64');
+   SP2_ltt=fread(fid,[ndelta,ndir],'float64');
+   SN1_ltt=fread(fid,[ndelta,ndir],'float64');
+   SN2_ltt=fread(fid,[ndelta,ndir],'float64');
+end
+
+
+if (nscalars>=7) 
+  time=fread(fid,1,'float64');
+  nx=fread(fid,1,'float64');
+  ny=fread(fid,1,'float64');
+  nz=fread(fid,1,'float64');
+  mu=fread(fid,1,'float64');   
+  ke=fread(fid,1,'float64');
+  epsilon=fread(fid,1,'float64'); 
+  eta = (mu^3 / epsilon)^.25;
+  delx_over_eta=(1/nx)/eta;
+end
+fclose(fid);
+
+
+
+% D_lll=SP_lll-SN_lll;
+% but to make SP and SN have the same sign as D, multipl
+% by -1 so:  D=SN-SP
+SP_lll=-SP_lll;
+SN_lll=-SN_lll;
+SP1_ltt=-SP1_ltt;
+SN1_ltt=-SN1_ltt;
+SP2_ltt=-SP2_ltt;
+SN2_ltt=-SN2_ltt;
+
