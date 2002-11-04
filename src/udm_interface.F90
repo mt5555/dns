2150aeca398057d7e7ee61d367bc9c773a7ebce8
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-11-04 17:36:25 +0000

    *** empty log message ***

--- src/udm_interface.F90
+++ src/udm_interface.F90
@@ -376,3 +376,189 @@ Q=0
 
 end subroutine
 
+
+
+
+
+
+
+
+
+
+
+subroutine udm_read_uvw_old(Q,Qhat,work1,bufudm)
+!
+! 
+!
+use params
+use mpi
+use fft_interface
+implicit none
+real*8 :: Q(nx,ny,nz,n_var)
+real*8 :: Qhat(nx,ny,nz,n_var)
+real*8 :: work1(nx,ny,nz)
+real*8 bufudm(nx2-nx1+1, ny2-ny1+1, nz2-nz1+1)
+
+
+!local
+character(len=80) message
+character(len=80) fname
+integer :: n
+
+integer fidudm, dsidudm, infoid, ierr
+integer i, j, k, i1, j1, k1
+integer nxstored, nystored, nzstored
+integer dimsudm 
+integer*8 sizesudm(3), gsizesudm(3), offsetsudm(3)
+
+character(len=80) fnameudm
+character(len=80) attrname 
+character(len=80) dsnameudm
+character(len=80) dotudm
+character*2 nochar 
+! character*(5) dbgname 
+
+#ifdef HAVE_UDM
+
+#include "UDMFCInterfaceF.h"
+
+
+Q=0
+
+   dotudm = '.'//char(0)
+   attrname = 'time'//char(0)
+   nochar(1:1) = char(0)
+   fnameudm = rundir(1:len_trim(rundir)) // 'restart.h5'//char(0)
+   call print_message("Restarting from UDM file:")
+   call print_message(fnameudm)
+   call UDM_FILE_OPEN(fnameudm, UDM_HDF_OPEN_READ, fidudm, ierr)
+
+   dotudm = '.'//char(0)
+   attrname = 'time'//char(0)
+   
+   call UDM_ATTR_READ(attrname,dotudm,fidudm,UDM_ATOMIC_DOUBLE,time_initial,ierr)
+
+   attrname = 'nx'//char(0)
+   call UDM_ATTR_READ(attrname,dotudm,fidudm,UDM_ATOMIC_INT, nxstored, ierr)
+   attrname = 'ny'//char(0)
+   call UDM_ATTR_READ(attrname,dotudm,fidudm,UDM_ATOMIC_INT, nystored, ierr)
+   attrname = 'nz'//char(0)
+   call UDM_ATTR_READ(attrname,dotudm,fidudm,UDM_ATOMIC_INT, nzstored, ierr)
+
+   if (nxstored .ne. ncpu_x *(nx2 - nx1 + 1))  &   
+       call abort("ERROR: data file nx <> nx set in params.h")
+   if (nystored .ne. ncpu_y *(ny2 - ny1 + 1))  & 
+       call abort("ERROR: data file ny <> ny set in params.h")
+   if (nzstored .ne. ncpu_z *(nz2 - nz1 + 1))  & 
+       call abort("ERROR: data file nz <> nz set in params.h")
+
+!  read g_xcord, g_ycor and g_zcord
+
+!   dsnameudm(1:12) = 'coords/xcord'
+!   dsnameudm(13:13) = char(0)
+!   call FIO_INFO_CREATE(fidudm,dsnameudm,UDM_INFOTYPE_DATASET,infoid,ierr) 
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_DIMS,   dimsudm,   ierr)
+!   if (dimsudm .ne. 1) call abort("ERROR: datafile g_xcord wrong")
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_GSIZES, sizesudm, ierr)
+!   offsetsudm(1) = 0
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_SIZES, sizesudm, ierr) 
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_OFFSETS, offsetsudm, ierr) 
+!   call FIO_READ(fidudm, dsnameudm, infoid, UDM_DOUBLE_DATASET, g_xcord(1), ierr)
+!   call FIO_INFO_FREE(1, infoid, ierr)
+!
+!   dsnameudm(1:12) = 'coords/ycord'
+!   call FIO_INFO_CREATE(fidudm,dsnameudm,UDM_INFOTYPE_DATASET,infoid,ierr)
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_DIMS,   dimsudm,   ierr)
+!   if (dimsudm .ne. 1) call abort("ERROR: datafile g_ycord wrong")
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_GSIZES, sizesudm, ierr)
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_SIZES, sizesudm, ierr)  
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_OFFSETS, offsetsudm, ierr)  
+!   call FIO_READ(fidudm, dsnameudm, infoid, UDM_DOUBLE_DATASET, g_ycord(1), ierr)
+!   call FIO_INFO_FREE(1, infoid, ierr)
+!
+!   dsnameudm(1:12) = 'coords/zcord'
+!   call FIO_INFO_CREATE(fidudm,dsnameudm,UDM_INFOTYPE_DATASET,infoid,ierr)
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_DIMS,   dimsudm,   ierr)
+!   if (dimsudm .ne. 1) call abort("ERROR: datafile g_zcord wrong")
+!   call FIO_INFO_ITEM_GET(infoid, UDM_INFO_GSIZES, sizesudm, ierr)
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_SIZES, sizesudm, ierr)  
+!   call FIO_INFO_ITEM_SET(infoid, UDM_INFO_OFFSETS, offsetsudm, ierr)  
+!   call FIO_READ(fidudm, dsnameudm, infoid, UDM_DOUBLE_DATASET, g_zcord(1), ierr)
+!   call FIO_INFO_FREE(1, infoid, ierr)
+
+   dsnameudm = 'ns_uvw/u'//char(0)
+   call UDM_INFO_CREATE(nochar, dsnameudm,fidudm,UDM_INFO_DATA_DIST, infoid, ierr)
+   CALL UDM_INFO_ITEM_GET(infoid, UDM_INFO_NDIMS, dimsudm, ierr)
+   if (dimsudm .ne. 3) call abort("ERROR: datafile is wrong")
+   CALL UDM_INFO_ITEM_GET(infoid, UDM_INFO_DIMS_TOTAL, gsizesudm, ierr)
+
+   if (gsizesudm(1) .ne. (ncpu_z *(nz2 - nz1 + 1)))    &
+       call abort("ERROR: nz, ncpu_z in param.h not consistent the datafile")
+   if (gsizesudm(2) .ne. (ncpu_y *(ny2 - ny1 + 1)))    &
+       call abort("ERROR: ny, ncpu_y in param.h not consistent the datafile")
+   if (gsizesudm(3) .ne. (ncpu_x *(nx2 - nx1 + 1)))    &
+       call abort("ERROR: nx, ncpu_x in param.h not consistent the datafile")
+
+   sizesudm(1) = gsizesudm(1) / ncpu_z  
+   sizesudm(2) = gsizesudm(2) / ncpu_y
+   sizesudm(3) = gsizesudm(3) / ncpu_x
+   if ( (gsizesudm(1) - sizesudm(1) * ncpu_z .ne. 0) .or.   &
+        (gsizesudm(2) - sizesudm(2) * ncpu_y .ne. 0) .or.   &
+        (gsizesudm(3) - sizesudm(3) * ncpu_x .ne. 0) )      &
+        call abort("ERROR: mod(nx, ncpu_x) != 0")
+   offsetsudm(1) = my_z * sizesudm(1)
+   offsetsudm(2) = my_y * sizesudm(2)
+   offsetsudm(3) = my_x * sizesudm(3)
+
+!   call UDM_INFO_ITEM_SET(infoid, UDM_INFO_NDIMS, dimsudm, ierr)
+   call UDM_INFO_ITEM_SET(infoid, UDM_INFO_DIMS, sizesudm, ierr)
+   call UDM_INFO_ITEM_SET(infoid, UDM_INFO_STARTS, offsetsudm, ierr)
+
+   CALL UDM_DATASET_READ(dsnameudm,fidudm,UDM_ATOMIC_DOUBLE,infoid,bufudm,ierr)
+
+   do k = nz1, nz2  
+   k1 = k - nz1 + 1  
+   do j = ny1, ny2   
+   j1 = j - ny1 + 1   
+   do i = nx1, nx2 
+   i1 = i - nx1 + 1  
+   Q(i,j,k,1) = bufudm(i1,j1,k1)
+   enddo
+   enddo
+   enddo
+   
+   dsnameudm = 'ns_uvw/v'//char(0)
+   CALL UDM_DATASET_READ(dsnameudm,fidudm,UDM_ATOMIC_DOUBLE,infoid,bufudm,ierr)
+   do k = nz1, nz2       
+   k1 = k - nz1 + 1 
+   do j = ny1, ny2      
+   j1 = j - ny1 + 1  
+   do i = nx1, nx2      
+   i1 = i - nx1 + 1 
+   Q(i,j,k,2) = bufudm(i1,j1,k1)
+   enddo
+   enddo
+   enddo
+ 
+   dsnameudm = 'ns_uvw/w'//char(0)
+   CALL UDM_DATASET_READ(dsnameudm,fidudm,UDM_ATOMIC_DOUBLE,infoid,bufudm,ierr)
+   do k = nz1, nz2       
+   k1 = k - nz1 + 1 
+   do j = ny1, ny2      
+   j1 = j - ny1 + 1  
+   do i = nx1, nx2      
+   i1 = i - nx1 + 1 
+   Q(i,j,k,3) = bufudm(i1,j1,k1)
+   enddo
+   enddo
+   enddo
+
+   call UDM_INFO_FREE(1,infoid,ierr)
+   call UDM_FILE_CLOSE(fidudm, ierr)
+
+
+#else
+   call abort("udm_write_uvw: Error: UDM support not compiled in")
+#endif
+
+end subroutine
