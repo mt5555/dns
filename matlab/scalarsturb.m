8703c9dd236d21b6a2f3c9d5c8ac92da1f17c40e
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2002-11-05 17:17:23 +0000

    *** empty log message ***

--- matlab/scalarsturb.m
+++ matlab/scalarsturb.m
@@ -1,43 +1,52 @@
 %
-%########################################################################
-%#  plot of DNS structure function output file
-%########################################################################
+%   matlab script to read DNS output files *.scalars-turb
 %
+% each file contains, in this order:
 %
+%   number of scalars in file
+%   time
+%   u_i,i^2    i=1,3
+%   u_i,i^3    i=1,3
+%   u_i,i^4    i=1,3
+%   wSw
+%   u_i^2      i=1,3 
+%   S2
+%   w_i^2      i=1,3
+%   w_i^3      i=1,3
+%   w_i^4      i=1,3
+%
+
 clear all;
-apply_fix=0;
 
-range=0:50;
-%range = 1
-%range=0:.5:2;
+name = '/ccs/scratch/taylorm/cj/cj19'
 
-fid=fopen('iso12_256_200.scalars-turb','r','b'); 
+nt=0;
+times=[0:.25:2.5];
+for t=times
+  tstr=sprintf('%10.4f',t+10000);
+  fname=[name,tstr(2:10),'.scalars-turb'];
+  disp(fname)
+  fid=fopen(fname,'r','l');
 
-
-while (1) 
   [ns_e,count] = fread(fid,1,'float64');
-  if (count~=1) break; end;
 
   time = fread(fid,1,'float64');
   data1 = fread(fid,[ns_e,1],'float64');
   data1=[time;data1];
-  if (nscalars_e==0) 
+  if (nt==0) 
     ints_e= data1;
   else
     ints_e= [ints_e,data1];
   end
-  nscalars_e=nscalars_e+1;
-  [nscalars_e,ns_e]
+  nt=nt+1;
+  [nt,ns_e]
 end
 fclose(fid);
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  the "expensive" scalars 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 time_e=ints_e(1,:);
-ux2=zeros([3,nscalars_e]);
+ux2=zeros([3,nt]);
 ux3=ux2;
 ux4=ux2;
 for i=1:3
@@ -52,26 +61,38 @@ if (ns_e>=13)
   end
 end
 if (ns_e>=14) 
-    S2=ints_e(14,:);                     % <S2>
+    S2=ints_e(15,:);                     % <S2>
 end
-
+if (ns_e>=23) 
+for i=1:3
+   vor2(i,:)=ints_e(i+15,:);    % < w^2 > 
+   vor3(i,:)=ints_e(i+18,:);    % < w^3 >
+   vor4(i,:)=ints_e(i+21,:);    % < w^4 >
+end
+end
 
 figure(6)
 clf
-hold on
 %plot(time_e,Sww)
+
+%plot(time_e,ux3(1,:)./ux2(1,:).^(3/2),'k')
+%hold on
+%plot(time_e,ux3(2,:)./ux2(2,:).^(3/2),'b')
+%plot(time_e,ux3(3,:)./ux2(3,:).^(3/2),'g')
 
 %plot(time_e,ux2(1,:),'k')
 %plot(time_e,ux2(2,:),'b')
 %plot(time_e,ux2(3,:),'g')
 
-plot(time_e,ux3(1,:)./ux2(1,:).^(3/2),'k')
-plot(time_e,ux3(2,:)./ux2(2,:).^(3/2),'b')
-plot(time_e,ux3(3,:)./ux2(3,:).^(3/2),'g')
 
-plot(time_e,ux4(1,:)./ux2(1,:).^2,'r')
-plot(time_e,ux4(2,:)./ux2(2,:).^2,'b')
-plot(time_e,ux4(3,:)./ux2(3,:).^2,'g')
+%plot(time_e,ux4(1,:)./ux2(1,:).^2,'r')
+%plot(time_e,ux4(2,:)./ux2(2,:).^2,'b')
+%plot(time_e,ux4(3,:)./ux2(3,:).^2,'g')
+
+plot(time_e,vor4(1,:)./vor2(1,:).^2,'r')
+hold on
+plot(time_e,vor4(2,:)./vor2(2,:).^2,'b')
+plot(time_e,vor4(3,:)./vor2(3,:).^2,'g')
 
 
 hold off
