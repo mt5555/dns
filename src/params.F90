d500da00ad4f93a0d6928e2d87f719eeff376b95
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2003-03-08 17:11:39 +0000

    *** empty log message ***

--- src/params.F90
+++ src/params.F90
@@ -226,7 +226,7 @@ integer,parameter :: nx=nxd,ny=nyd,nz=nzd      ! dimension of grid & data
 
 integer :: ndim       ! 2 or 3 dimensions.  ndim = (g_nz==1 ? 2 : 3)
 integer :: npassive = 0   ! number of passive scalars
-integer :: np1,np2        ! passive scalars Q(:,:,:,np1:np2)
+integer :: np1=1,np2=0    ! passive scalars Q(:,:,:,np1:np2)
 
 ! number of actual data points
 integer,parameter :: nslabx=nx2-nx1+1
