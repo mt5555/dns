99d69860cb078f6dad90abfc43ff51da2f3117c3
taylorm <taylorm@8957d3c6-3d43-44c2-9749-a0da8daff542>
2003-04-25 13:27:42 +0000

    *** empty log message ***

--- matlab/passivepdf.m
+++ matlab/passivepdf.m
@@ -12,22 +12,25 @@ fid=fopen('/scratch2/taylorm/tmix256C/tmix256C0001.0001.spdf','r','l');
 time1=fread(fid,1,'float64');
 npmax=fread(fid,1,'float64');         
 
+figure(1); clf; subplot(5,2,1)
 
 np=1;
 for p=1:npmax
    [n_del,delta,bin_size,n_bin,n_call,bins,pdf]=read1pdf(fid);
-   if (p==np) break; end;
+   %if (p==np) break; end;
+
+   s2=sum(pdf.*bins.^2);
+   mx=max(bins - bins.*(pdf==0));
+   mn=min(bins - bins.*(pdf==0));        % min over non zero values
+   
+   subplot(5,2,p) 
+   plot(bins,pdf)
+   ax=axis;
+   axis([-.5,1.5,ax(3),ax(4)]);
+   xlabel(sprintf('[%.3f,%.3f] <s^2>=%.5f',mn,mx,s2));
+   %title(sprintf('passive scalar  t=%.4f',time)); 
+   set(gca,'YTickLabel','')    
+
+   
+
 end
-
-
-
-figure(1); clf; subplot(1,1,1)
-s2=sum(pdf.*bins.^2);
-mx=max(bins - bins.*(pdf==0));
-mn=min(bins - bins.*(pdf==0));        % min over non zero values
-semilogy(bins,pdf)
-%  ax=axis;
-%  axis([0,5.0,ax(3),ax(4)]);
-xlabel(sprintf('[%.3f,%.3f] <s^2>=%.5f',mn,mx,s2));
-title(sprintf('passive scalar  t=%.4f',time)); 
-set(gca,'YTickLabel','')    
