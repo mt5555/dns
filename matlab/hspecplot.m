0a6586b7ee2c7bf7d461832c80116e850aa7037c
skurien <skurien@8957d3c6-3d43-44c2-9749-a0da8daff542>
2003-05-02 17:43:58 +0000

    routine to plot helicity spectra

---
+++ matlab/hspecplot.m
@@ -0,0 +1,71 @@
+%
+%########################################################################
+%#  plot of DNS helicity spectrum output file
+%########################################################################
+%
+
+%name = 'helicity_data/check256_hq_0030.0000';
+namedir = '/home2/skurien/';
+%name = 'helicity_data/sc1024_data/sc1024A0002.0000';
+%mu = 0.35e-4;
+
+name = 'dns/src/sk_helical0001.5000';
+mu = 5.0e-4;
+
+
+
+% plot all the spectrum:
+movie=1;
+
+
+spec_r_save=[];
+spec_r_save_fac3=[];
+
+fid=fopen([namedir,name,'.hspec'],'r','l');
+
+time=fread(fid,1,'float64')
+
+
+  n_r=fread(fid,1,'float64');
+  hspec_n=fread(fid,n_r,'float64');
+  hspec_p = fread(fid,n_r,'float64');
+scaling = 1;                       %2pi for Takeshi data, 1 otherwise
+
+  hspec_n = hspec_n*scaling;
+hspec_p = hspec_p*scaling;
+
+
+figure(20);
+loglog(abs(hspec_n),'r');  
+hold on;
+loglog(hspec_p,'b');
+loglog(abs(hspec_p+hspec_n),'k');
+loglog(abs(hspec_p-hspec_n),'k.-');
+grid
+hold off
+
+k = [1:n_r];
+figure(21)
+loglog(abs(hspec_n).*k'.^(4/3),'r');  
+hold on;
+loglog(hspec_p.*k'.^(4/3),'b');
+loglog(abs(hspec_p-hspec_n).*k'.^(4/3),'k.-');
+loglog(abs(hspec_p+hspec_n).*k'.^(4/3),'k');
+grid on;
+hold off
+
+figure(22)
+loglog(abs(hspec_n).*k'.^(5/3),'r');  
+hold on;
+loglog(hspec_p.*k'.^(5/3),'b');
+loglog(abs(hspec_p-hspec_n).*k'.^(5/3),'k.-');
+loglog(abs(hspec_p+hspec_n).*k'.^(5/3),'k');
+grid on;
+hold off
+
+
+%compute mean helicity
+H = sum(hspec_p+hspec_n)
+
+%compute dissipation rate of helicity
+h = sum((hspec_p+hspec_n).*k'.^2)*2*mu
