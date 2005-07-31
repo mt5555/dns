4a192d05353dcd74d35cf1faac1372fcdd2d17ba
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2005-07-31 18:08:42 +0000

    updated scalars.m to print R0

--- matlab/scalars.m
+++ matlab/scalars.m
@@ -7,6 +7,12 @@
 clear all;
 apply_fix=0;
 
+% defaults:
+nx=0;
+fcor=0;
+f_k=0;
+
+
 range=0:50;
 %range = 1
 %range=0:.5:2;
@@ -23,7 +29,7 @@ fid2=-1;
 %nx=1024;
 
 fid=endianopen('/home/mt/codes/dns_data/rotB0000.0000.scalars','r');
-nx=128;
+f_k= 16; fcor=40;
 
 
 
@@ -117,7 +123,7 @@ ke_diss_tot=[];
 Ea_diss_tot=[];
 h_diss_tot=[];
 ke_diss_tot2=[];
-j=0
+j=0;
 for i=2:l
   % skip repeats, and skip data with large KE increase
   % since it is probably due to decaying run re-adjustment:
@@ -140,26 +146,31 @@ Euse=Ea;
 
 epsilon=-(  ke_diss_d-mu*alpha^2*ints(1,:) );
 lambda=sqrt( mu*(2*Euse/3) / (epsilon/15)  );
-R_l = lambda.*sqrt(2*Euse/3)/mu;
-R_large = 1 * sqrt(2*Euse) / mu;
 eta = (mu^3 ./ abs(epsilon)).^(.25);
 
 epsilon_ke=-ke_diss_d;
 lambda_ke=sqrt( mu*(2*ke/3) / (epsilon_ke/15)  );
-R_l_ke=lambda_ke.*sqrt(2*ke/3)/mu;
+
+if (mu>0)
+  R_l = lambda.*sqrt(2*Euse/3)/mu;
+  R_large = 1 * sqrt(2*Euse) / mu;
+  R_l_ke=lambda_ke.*sqrt(2*ke/3)/mu;
+end
+
+
 
 
 disp(sprintf('max vor_z = %e',max(vor_z)));
 
-
-figure(2);  hold on;
-plot(time,R_l,'b'); hold on;
-plot(time,R_l_ke,'r'); hold on;
-title('R_\lambda');
-legend('R_{\lambda}', 'R_{\lambda}(total KE)')
-xlabel('time')
-print -djpeg -r72 rl.jpg
-
+if (mu>0)
+  figure(2);  hold on;
+  plot(time,R_l,'b'); hold on;
+  plot(time,R_l_ke,'r'); hold on;
+  title('R_\lambda');
+  legend('R_{\lambda}', 'R_{\lambda}(total KE)')
+  xlabel('time')
+  print -djpeg -r72 rl.jpg
+end
 
 figure(3);
 plot(time,lambda)
@@ -168,12 +179,13 @@ plot(time,lambda)
 print -djpeg -r72 lambda.jpg
 
 
-figure(4); subplot(1,1,1)
-plot(time,eta* nx*pi*2*sqrt(2)/3 )
-title('k_{nmax} \eta')
-xlabel('time')
-print -djpeg -r72 kmaxeta.jpg
-
+if (nx>0) 
+  figure(4); subplot(1,1,1)
+  plot(time,eta* nx*pi*2*sqrt(2)/3 )
+  title('k_{nmax} \eta')
+  xlabel('time')
+  print -djpeg -r72 kmaxeta.jpg
+end
 
 figure(5)
 clf
@@ -237,42 +249,49 @@ print -djpeg -r72 vor.jpg
 
 
 
-
-
-
-
-
-
-
-% averge eta to a number
-eta = eta(ceil(length(eta)/2):length(eta));
-eta = sum(eta)/length(eta);
-disp(sprintf('eta (average over last half of data) = %f ',eta));
-
-% averge R_l to a number
-R_l = R_l(ceil(length(R_l)/2):length(R_l));
-R_l = sum(R_l)/length(R_l);
-disp(sprintf('R_l (average over last half of data) = %f ',R_l));
-
-% averge R_l_ke to a number
-R_l_ke = R_l_ke(ceil(length(R_l_ke)/2):length(R_l_ke));
-R_l_ke = sum(R_l_ke)/length(R_l_ke);
-disp(sprintf('R_l_ke (average over last half of data) = %f ',R_l_ke));
-
-%disp(sprintf('1/250 in units of eta:  %f',(1/250)/eta));
-%disp(sprintf('1/500 in units of eta:  %f',(1/500)/eta));
-%disp(sprintf('1/512 in units of eta:            %f   %f', (1/512)/eta,eta*2*pi*512*sqrt(2)/3 )) ;
-%disp(sprintf('1/nx in units of eta:  %f',(1/nx)/eta));
-
 tturn=-2*ke./ke_diss_d;
 tturn = tturn(ceil(length(tturn)/2):length(tturn));
 tturn = sum(tturn)/length(tturn);
 disp(sprintf('eddy turnover time (averaged over last haf of data) = %f ',tturn));
 
 
-epsilon = epsilon(ceil(length(epsilon)/2):length(epsilon));
-epsilon = sum(epsilon)/length(epsilon);
-disp(sprintf('epsilon_E (averaged over last haf of data) = %f ',epsilon));
+% averge Smith's R0 to a number
+if (f_k>0) 
+  R0 = (ke_diss_f * (2*pi*f_k)^2 ).^(1/3) / (.5 * fcor);
+  R0 = R0(ceil(length(R0)/2):length(R0));
+  R0 = sum(R0)/length(R0);
+  disp(sprintf('Smith''s R0 (average over last half of data) = %f ',R0));
+end
+
+
+if (mu>0) 
+  % averge eta to a number
+  eta = eta(ceil(length(eta)/2):length(eta));
+  eta = sum(eta)/length(eta);
+  disp(sprintf('eta (average over last half of data) = %f ',eta));
+
+% averge R_l to a number
+  R_l = R_l(ceil(length(R_l)/2):length(R_l));
+  R_l = sum(R_l)/length(R_l);
+  disp(sprintf('R_l (average over last half of data) = %f ',R_l));
+  
+  % averge R_l_ke to a number
+  R_l_ke = R_l_ke(ceil(length(R_l_ke)/2):length(R_l_ke));
+  R_l_ke = sum(R_l_ke)/length(R_l_ke);
+  disp(sprintf('R_l_ke (average over last half of data) = %f ',R_l_ke));
+
+  disp(sprintf('1/250 in units of eta:  %f',(1/250)/eta));
+  disp(sprintf('1/500 in units of eta:  %f',(1/500)/eta));
+  disp(sprintf('1/512 in units of eta:            %f   %f', (1/512)/eta,eta*2*pi*512*sqrt(2)/3 )) ;
+  disp(sprintf('1/nx in units of eta:  %f',(1/nx)/eta));
+end
+
+
+if (alpha>0)
+  epsilon = epsilon(ceil(length(epsilon)/2):length(epsilon));
+  epsilon = sum(epsilon)/length(epsilon);
+  disp(sprintf('epsilon_E (averaged over last haf of data) = %f ',epsilon));
+end
 
 epsilon_ke = epsilon_ke(ceil(length(epsilon_ke)/2):length(epsilon_ke));
 epsilon_ke = sum(epsilon_ke)/length(epsilon_ke);
@@ -285,6 +304,7 @@ disp(sprintf('epsilon_f (averaged over last haf of data) = %f ',ke_diss_f));
 ke_diss_tot = ke_diss_tot(ceil(length(ke_diss_tot)/2):length(ke_diss_tot));
 ke_diss_tot = sum(ke_diss_tot)/length(ke_diss_tot);
 disp(sprintf('d(KE)/dt (averaged over last haf of data) = %f ',ke_diss_tot));
+
 
 print -depsc scalars.ps
 
