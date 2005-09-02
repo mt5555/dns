db9cb30a63bc13f1173035ef5550a2477709a6ac
skurien <skurien@8957d3c6-3d43-44c2-9749-a0da8daff542>
2005-09-02 16:50:38 +0000

    *** empty log message ***

--- src/analysis_isoave.F90
+++ src/analysis_isoave.F90
@@ -80,8 +80,8 @@ compute_hspec=.false.
 read_uvw=.false.
 !use_max_shear_direction=.true. !use to compute shear tensor with maximum S_1,2
 
-tstart=0.0
-tstop=5.0
+tstart=7.0
+tstop=7.0
 tinc=0.2
 icount=0
 
