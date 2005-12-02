bdd8ea538fc88dbc7e9a4e12600afc42e8389d33
wingate <wingate@8957d3c6-3d43-44c2-9749-a0da8daff542>
2005-12-02 20:04:01 +0000

    Added first draft of subroutine compute_pv_dissipation

--- src/aspect_diag.F90
+++ src/aspect_diag.F90
@@ -100,7 +100,127 @@ end subroutine
 
 
 
-subroutine compute_pv_dissipation()
+subroutine compute_pv_dissipation(q_diss,Q,Qhat,work1,work2,d1)
+use params
+use fft_interface
+!bw
+!bw  This routine computes the dissipation of Q = q^2/2
+!bw  
+!bw  enstr_diss = - nu < q laplacian (omega dot 
+!bw                                   grad (rho_o - b z + \tilde{rho}) >
+!bw  
+!bw               - kappa < q laplacian (grad \tilde{rho}
+!bw                                      dot  omega_a) >
+!bw  
+!bw  
+!bw  
+!bw  
+real*8 :: Q(nx,ny,nz,n_var)
+real*8 :: Qhat(g_nz2,nslabx,ny_2dz,n_var)
+real*8 :: work1(nx,ny,nz)
+real*8 :: work2(nx,ny,nz)
+real*8 :: d1(nx,ny,nz)
+real*8 :: vor(nx,ny,nz,3)
+real*8 :: potvor(nx,ny,nz)
+real*8 :: dummy(1)
+real*8 :: enstr_diss, omegadotrho_nu(nx,ny,nz), omegadotrho_kappa(nx,ny,nz)
+integer :: pv_type, i, j, k, im, jm, km
+
+
+!
+! For this subroutine pv_type = 1 (the full potential vorticity)
+!
+
+!bw
+!bw I probably don't have the work arrays correct in these calls.
+!bw
+call vorticity(Q,vor,work1,work2)
+call potential_vorticity(potvor,vor,Q,d1,work,pv_type)
+!bw
+!bw
+!bw Compute this quantity in 3 stages in physical space
+!bw   1. omega dot grad \tilde{rho}
+!bw   2. omega_3 b
+!bw   3. omega_3 f
+!bw Then fft, then
+!bw
+
+omegadotrho_nu = 0
+
+   call der(Q(1,1,1,4),d1,dummy,work1,DX_ONLY,1)
+   do k=nz1,nz2
+      do j=ny1,ny2
+         do i=nx1,nx2
+            omegadotrho_nu(i,j,k) = omegadotrho_nu(i,j,k) &
+                              + d1(i,j,k)*vor(i,j,k,1)
+         enddo
+      enddo
+   enddo
+  ! compute theta_y
+   call der(u(1,1,1,4),d1,dummy,work,DX_ONLY,2)
+   do k=nz1,nz2
+      do j=ny1,ny2
+         do i=nx1,nx2
+            omegadotrho_nu(i,j,k) = omegadotrho_nu(i,j,k) + d1(i,j,k)*vor(i,j,k,2)
+         enddo
+      enddo
+   enddo
+ 
+omegadotrho_kappa(i,j,k) = omegadotrho_nu(i,j,k)
+  
+   ! compute theta_z -- do not forget to add in coriolis here
+   call der(u(1,1,1,4),d1,dummy,work,DX_ONLY,3)
+   do k=nz1,nz2
+      do j=ny1,ny2
+         do i=nx1,nx2
+            omegadotrho_nu(i,j,k) = omegadotrho_nu(i,j,k) + &
+                            d1(i,j,k)/Lz*(vor(i,j,k,3)-bous)
+            omegadotrho_kappa(i,j,k) = omegadotrho_kappa(i,j,k) + &
+                                d1(i,j,k)/Lz*(vor(i,j,k,3)+fcor)
+         enddo
+      enddo
+   enddo
+!bw 
+!bw Now laplacian both omegadotrho_nu  and omegadotrho_kappa
+!bw
+   call fft3d(omegadotrho_nu,work1)
+   call fft3d(omegadotrho_kappa,work1)
+!bw
+   do k=nz1,nz2
+      km=(kmcord(k))
+      do j=ny1,ny2
+         jm=(jmcord(j))
+         do i=nx1,nx2
+            im=(imcord(i))
+            rwave = im**2 + jm**2 + (km/Lz)**2
+            omegadotrho_nu(i,j,k) = -omegadotrho_nu(i,j,k)*rwave
+            omegadotrho_kappa(i,j,k) = -omegadotrho_kappa(i,j,k)*rwave
+         enddo
+      enddo
+   enddo
+!bw Transform back -- I hope this is the right way to do this.
+   call ifft3d(omegadotrho_nu,work1)
+   call ifft3d(omegadotrho_kappa,work1)
+!bw Now take the area average
+   enstr_diss = 0
+   do k=nz1,nz2
+      km=(kmcord(k))
+      do j=ny1,ny2
+         jm=(jmcord(j))
+         do i=nx1,nx2
+            im=(imcord(i))
+            xfac = 8
+            if (km==0) xfac=xfac/2
+            if (jm==0) xfac=xfac/2
+            if (im==0) xfac=xfac/2
+            enstr_diss = enstr_diss &
+                         + nu*omegadotrho_nu(i,j,k) &
+                         + kamma*omegadotrho_kappa(i,j,k)
+         enddo
+      enddo
+   enddo
+
+
 end subroutine
 
 
