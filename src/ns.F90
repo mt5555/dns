9030d95f11f56d098a4ba5073325b894ca8dcb53
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2005-12-11 15:16:43 +0000

    *** empty log message ***

--- src/ns.F90
+++ src/ns.F90
@@ -511,7 +511,7 @@ do j=1,ny_2dz
 !               ens_diss = ens_diss + 2*xfac*(mu*xw* (1+xw*alpha**2)**2)*  &
 !                    ((wy-vz)**2 + (uz-wx)**2 + (vx-uy)**2) 
                else
-                  ens_diss = ens_diss + 2*xfac*mu*xw_viss)*  &
+                  ens_diss = ens_diss + 2*xfac*mu*xw_viss*  &
                        ((wy-vz)**2 + (uz-wx)**2 + (vx-uy)**2) 
                endif
                
