590e6c96ee107015540c23ba536d760f930761f1
wingate <wingate@8957d3c6-3d43-44c2-9749-a0da8daff542>
2006-03-09 23:26:44 +0000

    updated fftops for pv

--- src/fftops.F90
+++ src/fftops.F90
@@ -382,7 +382,7 @@ real*8 dummy(1)
 
 if (n_var<3) call abort("potential vorticity() requires n_var>2")
 
-call vorticity(Q,vor,work1,work2)
+call vorticity(u,vor,d1,work)
    
 if(pv_type == 1) then
    !bw
