d1ce1716f9c8255f1e276b0207a234a5bbfd0721
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2006-04-28 14:31:35 +0000

    documentation on Kerr's compression algorith, and downsampling

--- src/convert.F90
+++ src/convert.F90
@@ -70,7 +70,7 @@ tstop=5.0
 tinc=.5
 
 ! to read times from  file times.dat:
-! tstart=-1; tinc=0; tname="times.dat"
+  tstart=-1; tinc=0; tname="times.dat"
 
 ! these lines are modifed by some sed scripts for automatic running
 ! of this code by putting in new values of tstart, tstop, tinc,
