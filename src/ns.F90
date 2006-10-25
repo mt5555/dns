3bf4686bb0c10012c23bf8d5dc2a76fa7bde0e9a
emanalo <emanalo@8957d3c6-3d43-44c2-9749-a0da8daff542>
2006-10-25 19:17:56 +0000

    added code for bardina-alpha

--- src/ns.F90
+++ src/ns.F90
@@ -660,6 +660,9 @@ do j=1,ny_2dz
          if (alpha_B>0) then
             ! apply H^{-1}
             ! divide rhs(:,:,:,:) by  (1 + alpha_B**2(2*pi*k)**2)
+            rhs(k,i,j,1) = rhs(k,i,j,1)/(1+xw*alpha_B**2) 
+            rhs(k,i,j,2) = rhs(k,i,j,2)/(1+xw*alpha_B**2)
+            rhs(k,i,j,3) = rhs(k,i,j,3)/(1+xw*alpha_B**2)
          endif
 
       enddo
