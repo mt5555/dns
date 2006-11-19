b22e7f4d37ed64c4941f40e21b0a7cbcd0ac1743
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2006-11-19 05:26:39 +0000

    adding fftw interface

--- src/fftw_interface.F90
+++ src/fftw_interface.F90
@@ -2,7 +2,7 @@
 #if 0
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! our wrapper for AMD ACML FFT 
+! our wrapper for ECMWF FFT99 
 ! 
 !
 ! provides public interfaces:
@@ -14,46 +14,8 @@
 ! Size of the grid point data         p(1:n1,1:n2,1:n3)
 ! Size of fourier coefficients        p(1:n1+2,1:n2+2,1:n3+2)
 !
-ACML representation:
 
-f=[fhat(1) + 2 fhat(2*m+1) cos(m*2pi*x) - 2*fhat(2m+2) sin(m*2pi*x) ] / sqrt(n)
-
-X(i)   = real part of Z(i) i=0.. N/2
-X(N-i) = imag part of Z(i) i=1.. (N-1)/2
-
- grid space data:    1 2 3 4 5 6 7 8 * *
-
- i                   1 2 3 4 5 6 7 8
- fourier space:      0 1 2 3 4 3 2 1 * *
- rearranged:         0 4 1 1 2 2 3 3 * *
-
-xout(1)=x(1)
-xout(2)=x(n/2+1)
-do i=2,n/2
-   xout(2*i-1) = x(i) 
-   xout(2*i)   = x(n-i+2)
-enddo
-
-
-NOTE: ACML has multiple real FFT, but it doesnt have an "mode"
-argument and it requires no padding (i.e. real*8 X(N,M))
-
-integer :: info,n,mode
-real*8 :: X(n)
-real*8 :: comm(3*N+100)
-mode=0:  default initialization
-mode=1:  real transform is perfmed
-mode=2:  init + transform
-mode=100:  expensive init
-
-
-   CALL DZFFT(0,N,X,COMM,INFO)  ! init
-   CALL DZFFT(1,N,X,COMM,INFO)  ! forward transform
-   DO 10 I = N/2+2, N           ! prep for inverse transform
-      X(I) = -X(I)
-10 CONTINUE
-   CALL ZDFFT(2,N,X,COMM,INFO)  ! inverse transform
-
+FFT data representation:
 
 sum over m=1..n/2:
 
@@ -83,6 +45,22 @@ In otherwords:
  rearranged:         0 4 1 1 2 2 3 3 * *
 
 
+
+fftw_plan fftw_plan_many_dft_r2c(int rank, const int *n, int howmany,
+                                      double *in, const int *inembed,
+                                      int istride, int idist,
+                                      fftw_complex *out, const int *onembed,
+                                      int ostride, int odist,
+                                      unsigned flags);
+fftw_plan fftw_plan_many_dft_c2r(int rank, const int *n, int howmany,
+                                      fftw_complex *in, const int *inembed,
+                                      int istride, int idist,
+                                      double *out, const int *onembed,
+                                      int ostride, int odist,
+                                      unsigned flags);
+
+
+
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #endif
 
@@ -90,32 +68,14 @@ In otherwords:
 module fft_interface
 use params
 implicit none
-integer, parameter ::  num_fftsizes=3
+include 'fftw3.f'
 
-integer :: init=0
-type fftdata_d
-   real*8,dimension(:),pointer :: trigs1
-   real*8,dimension(:),pointer :: trigs2
-   integer :: size
-end type
-type(fftdata_d) :: fftdata(num_fftsizes)
-
-
-private :: fftinit, getindex
 contains 
 
 
 
 
 subroutine fft_interface_init()
-
-integer i
-real*8 :: one=1
-
-init=1
-do i=1,num_fftsizes
-   fftdata(i)%size = 0	
-enddo
 end subroutine
 
 
@@ -144,66 +104,6 @@ end subroutine
 
 
 
-
-
-subroutine fftinit(n,index)
-integer n,index,info
-character(len=80) message
-
-if (init==0) call abortdns("fftacml_interface.F90: call fft_interface_init to initialize first!")
-if (n>1000000) call abortdns("fftacml_interface.F90: n>1 million")
-if (n<0) call abortdns("Error: invalid value of n for fft")
-
-fftdata(index)%size=n
-allocate(fftdata(index)%trigs1(3*n + 100))
-allocate(fftdata(index)%trigs2(3*n + 100))
-
-write(message,'(a,i6)') 'Initializing ACML DZFFT of size n=',n
-call print_message(message)
-
-call DZFFTM(100,n,dummy,fftdata(index)%trigs1,info)
-
-write(message,'(a,i6)') 'Initializing ACML ZDFFT of size n=',n
-call print_message(message)
-
-call ZDFFTM(100,n,dummy,fftdata(index)%trigs2,info)
-
-write(message,'(a)') 'Initializing ACML done'
-call print_message(message)
-
-
-end subroutine
-
-
-
-
-subroutine getindex(n1,index)
-integer n1,index
-
-character(len=80) message_str
-integer i,k
-
-
-i=0
-do 
-   i=i+1
-   if (i>num_fftsizes) then
-      write(message_str,'(a,i10)') "fft_interface.F90:  Failed initializing an fft of size =",n1
-      call abortdns(message_str)
-   endif
-
-   if (fftdata(i)%size==0) then
-      call fftinit(n1,i)      
-      exit 
-   endif
-   if (n1==fftdata(i)%size) exit 
-enddo
-index=i
-end subroutine
-
-
-
-
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! Compute 3D in-place iFFT of p
@@ -223,23 +123,30 @@ call wallclock(tmx1)
 if (tims(19)==0) ncalls(19)=0  ! timer was reset, so reset counter too
 
 if (n1==1) return
-ASSERT("ifft1: dimension too small ",n1<=n1d)
-call getindex(n1,index)
+ASSERT("ifft1: dimension too small ",n1+2<=n1d)
+ASSERT("ifft1: dimension too small ",n2==n2d)
 
+! number of FFTs to compute:  n2*n3
+! stride:  n1d
 
-
-j=0  ! j=number of fft's computed for each k
 do k=1,n3
    do j=1,n2
 
       ! move the last cosine mode back into correct location:
       p(n1+1,j,k)=p(2,j,k)
-      !p(2,j,k)=0             ! not needed?
-      !p(n1+2,j,k)=0          ! not needed?
+      p(2,j,k)=0             ! not needed?
+      p(n1+2,j,k)=0          ! not needed?
    enddo
-
-   call fft991(p(1,1,k),w,fftdata(index)%trigs,fftdata(index)%ifax,1,n1d,n1,n2,1)
 enddo
+
+flags = 
+
+!call fft991(p(1,1,k),w,fftdata(index)%trigs,fftdata(index)%ifax,1,n1d,n1,n2,1)
+call fftw_plan_many_dft_c2r(int rank, n1,n2*n3,p, const int *inembed?,
+                       n1d, int idist?, p, const int *onembed?,
+                       n1d, int odist?, flags);
+
+
 
 call wallclock(tmx2) 
 tims(19)=tims(19)+(tmx2-tmx1)          
@@ -267,7 +174,7 @@ if (tims(18)==0) ncalls(18)=0  ! timer was reset, so reset counter too
 
 
 if (n1==1) return
-ASSERT("fft1: dimension too small ",n1<=n1d)
+ASSERT("fft1: dimension too small ",n1+2<=n1d)
 call getindex(n1,index)
 
 scale=n1
