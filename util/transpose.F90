08a64e27b114367acce54d4675b248acc84e1671
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2006-11-30 17:08:02 +0000

    *** empty log message ***

---
+++ util/transpose.F90
@@ -0,0 +1,143 @@
+#if 0
+compile with:
+
+ftn -target=catamount -byteswapio -fast -r8 -Mpreprocess -DMESHSIZE=512
+-DPROCY=4 -DPROCZ=4 -o ./transpose.x transpose.f90
+
+on Redstorm:
+ 
+4096^3 on 8192 cores:  (I dont have a run like this)
+mpif90  -fast -r8 -Mpreprocess -DMESHSIZE=4096 \
+-DPROCY=2 -DPROCZ=4096 -o ./transpose.x transpose.F90
+yod -VN -sz 8192  transpose.x
+
+4096^3 on 16384 cores: 
+
+mpif90  -fast -r8 -Mpreprocess -DMESHSIZE=4096 \
+-DPROCY=128 -DPROCZ=128 -o ./t2.x transpose.F90
+
+
+yod -VN -sz 16384  transpose.x 
+
+
+ 
+
+
+
+"bandwidth"   8*MESHSIZE^3 / NPROC / time_in_seconds
+
+
+#endif
+
+program test_transpose
+implicit none
+
+! This code is for pencils, in y and z directions. 
+#define PROCX 1 
+
+!Problem size and processor counts. To be #defined
+!No divisibility tests performed here
+integer, parameter :: nxg=MESHSIZE, nyg=MESHSIZE, nzg=MESHSIZE
+integer, parameter :: npx = PROCX, npy=PROCY, npz=PROCZ
+integer, parameter :: nproc = npx*npy*npz
+integer, parameter :: nx = nxg/npx, ny = nyg/npy, nz=nzg/npz
+
+include 'mpif.h'
+integer my_rank
+integer ycomm, zcomm
+integer yid, zid
+
+integer ierr, idummy, nproc2
+real dummy
+
+real *8 :: ux(nxg, ny, nz)            !  decmop:  1xPROCYxPROCZ
+real *8 :: uy(nxg/PROCY, nyg, nz)     !  decomp   PROCYx1xPROCZ 
+
+real*8 :: spack(nxg*ny*nz), rpack(nxg*ny*nz)
+
+integer bsize
+integer i, j, k, n
+
+real*8 :: tmx3,tmx4,tmx2,tmx1,tmx_max,tmx_min
+real*8 :: a2a_max, a2a_min
+real*8 :: error, error_max, error_max_g
+
+call mpi_init(ierr)
+call mpi_comm_rank(MPI_COMM_WORLD,my_rank,ierr)
+call mpi_comm_size(MPI_COMM_WORLD,nproc2,ierr)
+if (nproc2 /= nproc) then
+   if(my_rank .eq. 0) print *,'error: nproc,nproc2=',nproc,nproc2
+   call mpi_abort(MPI_COMM_WORLD,1,ierr)
+endif
+
+!Form y and z communicators
+zid = my_rank/npy
+yid = mod(my_rank, npy)
+
+call mpi_comm_split(mpi_comm_world, zid, my_rank, ycomm, ierr)
+call mpi_comm_split(mpi_comm_world, yid, my_rank, zcomm, ierr)
+
+!Fill with data that will allow me to debug the transpose operation
+do k = 1, nz
+  do j = 1, ny
+    do i = 1, nx
+      ux(i,j,k) = 1e8*real(i)+1e4*real(yid*ny+j)+1e0*real(zid*nz+k)
+    end do
+  end do
+end do
+
+tmx1 = MPI_Wtime()
+
+!Pack data for alltoall in y-direction
+bsize = nxg*ny*nz/npy
+do n = 0, npy-1
+  spack(n*bsize+1:(n+1)*bsize) = & 
+    reshape(ux(n*nxg/npy+1:(n+1)*nxg/npy, 1:ny, 1:nz), (/bsize/))
+end do
+
+tmx2 = MPI_Wtime()
+
+call MPI_Alltoall(spack,bsize,MPI_REAL8,rpack,bsize,MPI_REAL8,ycomm,ierr)
+
+tmx3 = MPI_Wtime()
+
+!Unpack the data
+do n = 0, npy-1
+  uy(1:nxg/PROCY, n*ny+1:(n+1)*ny, 1:nz) = &
+    reshape(rpack(n*bsize+1:(n+1)*bsize), (/nxg/PROCY, ny, nz/) ) 
+end do
+
+tmx4 = MPI_Wtime()
+
+!DEBUG -  if transpose was done correct
+!error_max = 0.0
+!do k = 1, nz
+!  do j = 1, nyg
+!    do i = 1, nxg/PROCY
+!      error = abs(uy(i,j,k)-1e8*real(yid*nxg/PROCY+i)-1e4*real(j)-1e0*real(zid*nz+k))
+!      if(error .gt. error_max) error_max = error
+!    end do
+!  end do
+!end do 
+!call MPI_Reduce(error_max,error_max_g,1,MPI_REAL8,MPI_MAX,0,MPI_COMM_WORLD,ierr)
+!if(my_rank .eq. 0) print *, 'maximum error is', error_max_g
+
+tmx4=tmx4-tmx1
+call MPI_Reduce(tmx4,tmx_max,1,MPI_REAL8,MPI_MAX,0,MPI_COMM_WORLD,ierr)
+call MPI_Reduce(tmx4,tmx_min,1,MPI_REAL8,MPI_MIN,0,MPI_COMM_WORLD,ierr)
+
+tmx4=tmx3-tmx2
+call MPI_Reduce(tmx4,a2a_max,1,MPI_REAL8,MPI_MAX,0,MPI_COMM_WORLD,ierr)
+call MPI_Reduce(tmx4,a2a_min,1,MPI_REAL8,MPI_MIN,0,MPI_COMM_WORLD,ierr)
+
+if(my_rank==0) then
+   print *,'Min alltoall : (in min)',a2a_min/60.0
+   print *,'Max alltoall : (in min)',a2a_max/60.0
+   print *,'Min alltoall + copy time: (in min)',tmx_min/60.0
+   print *,'Max alltoall + copy time: (in min)',tmx_max/60.0
+endif
+
+call MPI_Finalize(ierr)
+
+end program test_transpose
+
