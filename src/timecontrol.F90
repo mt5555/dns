b4dc473f85441927fa2114b824b38a7cc54c4e2f
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2007-02-21 23:20:16 +0000

    *** empty log message ***

--- src/timecontrol.F90
+++ src/timecontrol.F90
@@ -76,7 +76,7 @@ time_target=time_final
 !              dt < .31  / (  mu ( delx**-2 + dely**-2 + delz**-2 ) )
 !             
 !
-umax=maxs(4)+fcor/pi
+umax=maxs(4)+(bous+fcor)/pi
 
 if (ndim==3) then
    mumax = mu*(delx**(-2)) + &
