4ce8761cb985fce546ae8cdab5d6ab8b462c7ca9
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2008-01-07 06:21:29 +0000

    more debugging...

--- testing/dealias.sh
+++ testing/dealias.sh
@@ -2,22 +2,26 @@
 ./gridsetup.py 1 1 2 32 32 32
 make -j 2 dns
 
-
+#
+# fft-dealias >=:  -.17e-5  -.56e-7  -.19e-8  -.66e-10 -.82e-11 -.15e-10
+#             >    -.10e-4  -.34e-6  -.11e-7  -.41e-9  -.21e-10 -.28e-10
+# fft-sphere    :  -.19e-2  -.58e-4  -.17e-5  -.47e-7  -.16e-8  -.94e-10
 
 set i=0
 loop:
 
-if ( $i == 1 ) set method = "fft-dealias"
-if ( $i == 0 ) set method = "fft-sphere"
+if ( $i == 0 ) set method = "fft-dealias"
+if ( $i == 1 ) set method = "fft-sphere"
 if ( $i == 2 ) set method = "fft-phase"
 
-set dt = .005
+set dt = 0.005
 echo $method :  dt = $dt
 rm -f /tmp/temp.out /tmp/dealias*inp
 sed s/DT/$dt/ ../testing/dealias.inp | \
 sed s/METHOD/"$method"/  > /tmp/dealias.inp
 #mpirun -np 2 ./dns -i /tmp/dealias.inp -d /tmp | tee  /tmp/temp.out
 mpirun -np 2 ./dns -i /tmp/dealias.inp -d /tmp >   /tmp/temp.out
+grep "ke:" /tmp/temp.out
 grep "entire run:" /tmp/temp.out
 grep "per timestep, min/max" /tmp/temp.out
 
@@ -49,7 +53,7 @@ grep "entire run:" /tmp/temp.out
 grep "per timestep, min/max" /tmp/temp.out
 
 
-set dt = .0003125
+set dt = .000001
 echo $method :  dt = $dt
 rm -f /tmp/temp.out /tmp/dealias*inp
 sed s/DT/$dt/ ../testing/dealias.inp | \
@@ -59,14 +63,9 @@ grep "entire run:" /tmp/temp.out
 grep "per timestep, min/max" /tmp/temp.out
 
 
-set dt = .00015626
-echo $method :  dt = $dt
-rm -f /tmp/temp.out /tmp/dealias*inp
-sed s/DT/$dt/ ../testing/dealias.inp | \
-sed s/METHOD/"$method"/  > /tmp/dealias.inp
-mpirun -np 2 ./dns -i /tmp/dealias.inp -d /tmp >  /tmp/temp.out
-grep "entire run:" /tmp/temp.out
-grep "per timestep, min/max" /tmp/temp.out
+
+echo
+echo 
 
 
 
