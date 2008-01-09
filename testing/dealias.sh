e86d26d178ccae78c8a304e38b1ab69049683335
mt <mt@8957d3c6-3d43-44c2-9749-a0da8daff542>
2008-01-09 14:58:18 +0000

    more debugging of fft-phase. seems to be working for powers of 2 and 5, but not 3.

--- testing/dealias.sh
+++ testing/dealias.sh
@@ -17,7 +17,7 @@
 #
 
 
-set NCPU = 1
+set NCPU = 2
 
 if ( $NCPU == 1 ) then
    set MPIRUN = " "
@@ -25,17 +25,28 @@ else
    set MPIRUN = "mpirun -np $NCPU "
 endif
 
-#./gridsetup.py 1 1 $NCPU 64 64 64
-#make -j2 convert ; mv convert convert.64
 
-./gridsetup.py 1 1 $NCPU 32 32 32
-make -j2 convert ; mv convert convert.32
+# on M grid, modes 2N-M will alias
+# contaiminated = M-(2N-M) = 2(M-N)
+# we want 2(M-N) >= N      2M >= 3N  M>= 3N/2
+set M = 72   # allows for N<=48
+set N = 40
+
+# debug: fft-phase bad for 24,48, okay for 32,40,20
+
+if ! (-x convert.big)  then
+  ./gridsetup.py 1 1 1 $M $M $M
+  make -j2 convert ; mv convert convert.big
+endif
+
+./gridsetup.py 1 1 $NCPU $N $N $N
+make -j2 convert ; mv convert convert
 
 
 
-#set method = "fft-dealias"
-#set method = "fft-23sphere"
 #set method = "fft-sphere"
+#set method = "fft-23sphere"
+#set method = "fft-dealias"
 set method = "fft-phase"
 
 echo $method 
@@ -43,15 +54,29 @@ rm -f /tmp/temp0000.0000* /tmp/dealias*inp /tmp/temp1.out /tmp/temp2.out
 sed s/METHOD/"$method"/  ../testing/dealias.inp > /tmp/dealias.inp
 
 
-$MPIRUN ./convert.32 -smax 999 -so -cout nlout  -i /tmp/dealias.inp -d /tmp  | tee /tmp/temp1.out
-$MPIRUN ./convert.64 -si -cout nlin  -i /tmp/dealias.inp -d /tmp   | tee /tmp/temp2.out
-
+$MPIRUN ./convert -smax 999 -so -cout nlout  -i /tmp/dealias.inp -d /tmp  | tee /tmp/temp1.out
+./convert.big -si -cout nlin  -i /tmp/dealias.inp -d /tmp   | tee /tmp/temp2.out
+echo 
+echo =======================================================
+echo "Initial condition code N=$N"
 grep "number of retained modes:" /tmp/temp1.out
+echo "Errors compared to N=$M calculation:"
 grep "number of modes" /tmp/temp2.out
 grep "number of non-zero" /tmp/temp2.out
 grep "max error over " /tmp/temp2.out
 
 exit
+
+RESULTS: (for N=32, M=64.   M=72 zero is around 1e-17)
+               # modes      
+               tested     error
+             
+fft-sphere     14363      2.5e-3     aliasing error in waves 2-15
+fft-23dealias   5041      1e-18
+fft-dealais     9261      2e-18
+fft-phase      14363      3e-18
+
+                      
 
 notes: 
 fft-sphere:  keeping up to 15
