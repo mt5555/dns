2a1fe3691fc8a2f364a9f6a1f0664ea7324bf9d6
zhujz <zhujz@8957d3c6-3d43-44c2-9749-a0da8daff542>
2008-01-31 22:18:58 +0000

    *** empty log message ***

--- matlab/pdfplot.m
+++ matlab/pdfplot.m
@@ -7,12 +7,12 @@
 %########################################################################
 %
 %
-clear all;
+clear all;close all;
 
 
-dp=2;    % which seperation length index to plot
 
-fid=fopen('/home/mataylo/codes/dns/src/temp0002.0000.sf','r','l');
+
+fid=fopen('../temp0002.0000.sf','r','l');
 
 components=['u','v','w'];
 var=['x','y','z'];
@@ -23,26 +23,34 @@ var=['x','y','z'];
 
   % LONGITUDINAL PDFS  
   for j=1:3
-    for i=1:3  
+    for i=1:3
       % var(i) componenents(j)
       [n_del1,delta1,bin_size1,n_bin1,n_call1,bins1,pdf1]=read1pdf(fid);
+
+      for dp=1:n_del1;    % which seperation length index to plot
+      %delta1(dp)          
       % ignore the i<>j PDFs
       if (i==j)
-        subplot(3,1,i)
-        semilogy(bins1(:,dp),pdf1(:,dp))
-        title(sprintf('time=%f',time1));
-        ylabel(['\Delta',sprintf('_{  %i%s} %s',delta1(dp),var(i),components(j))]);
-        set(gca,'YTickLabel','')
+        %subplot(3,1,i)
+        semilogy(bins1(:,1),pdf1(:,dp)./bin_size1(dp))
+        sum(pdf1)
+        hold on;
+        %title(sprintf('time=%f',time1));
+        %ylabel(['\Delta',sprintf('_{  %i%s} %s',delta1(dp),var(i),components(j))]);
+        %set(gca,'YTickLabel','')
       end
+      end;
     end
   end
-  
+  hold on;
   % sometimes higher order PDFs are also in the input file,
   % skip them.  we've read 3 PDFs so far, so skip the rest:
   for i=[4:nSF]
     for j=1:3
       % these are the 3rd and 4th order SF 
       [n_del1,delta1,bin_size1,n_bin1,n_call1,bins1,pdf1]=read1pdf(fid);
+        %semilogy(bins1(:,1),pdf1(:,dp))
+        hold on;      
     end
   end
 
