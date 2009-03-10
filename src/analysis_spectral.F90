0e6612589997d4ed8010a4d79a2ed54a4eb817de
skurien <skurien@8957d3c6-3d43-44c2-9749-a0da8daff542>
2009-03-10 20:29:42 +0000

    *** empty log message ***

--- src/analysis_spectral.F90
+++ src/analysis_spectral.F90
@@ -97,9 +97,11 @@ read_uvw=.false.
 compute_hfree=.false.		!extracting helicity-free modes
 project_ch=.true.               !Craya-Herring projection and spectra
 
+
 tstart=0.0
 tstop=2.0
 tinc=0.2
+
 icount=0
 
 nxdecomp=1
