37f9edf327c30e59303879ee365f2b65a1284d97
admin <admin@8957d3c6-3d43-44c2-9749-a0da8daff542>
2010-02-26 20:47:11 +0000

    read in 64MB of data at a time

--- util/interleave.py
+++ util/interleave.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from string import *
+from cStringIO import StringIO
 import os, commands, getopt, sys
 
 
@@ -12,22 +13,38 @@ fv = open(filenamev,mode='rb')
 fw = open(filenamew,mode='rb')
 fi = open('interleave.uvw',mode='wb')
 
+
+
 i=0
+ntot = 0
+bsize = 8*1024*1024   # read 64MB at a time.  need 384MB to run
 while 1:
     i=i+1
-    if (i % 100000 == 0 ):
-        print 'processing N=%i' % i
-    data = fu.read(8)
-    if (data == '' ):
+    print 'ntot = %i' % ntot
+    datau = fu.read(8*bsize)
+    datav = fv.read(8*bsize)
+    dataw = fw.read(8*bsize)
+    nread = len(datau)
+    if ( nread % 8 != 0):
+        print 'Error reading file - did not contain multiple of 8 bytes'
+        sys.exit(0)
+    nread = nread/8
+    ntot += nread
+    if (nread == 0 ):
         print 'hit EOF. stopping'
-        print 'processed N=%i numbers from each file' %i
+        print 'processed N=%i real*8 numbers from each file' % ntot
         fi.close() 
         sys.exit(0)
-    fi.write(data)
-    data = fv.read(8)
-    fi.write(data)
-    data = fw.read(8)
-    fi.write(data)
+
+    outbuf=StringIO()
+    for j in range(nread):
+        j1 = j*8
+        j2 = j1 + 8
+        outbuf.write(datau[j1:j2])
+        outbuf.write(datav[j1:j2])
+        outbuf.write(dataw[j1:j2])
+    
+    fi.write(outbuf.getvalue())
     
 
 
