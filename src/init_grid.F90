755bbf3626b4d36811b42f26fa53c836b49a6c7e
admin <admin@8957d3c6-3d43-44c2-9749-a0da8daff542>
2010-03-04 19:39:32 +0000

    addeding regular viscosity implicit option

--- src/init_grid.F90
+++ src/init_grid.F90
@@ -3025,6 +3025,10 @@ else if (sdata=='hyper16_imp') then
    hyper_type=1
    mu_hyper=8
    mu_hyper_value = rvalue
+else if (sdata=='hyper2_fix') then
+   hyper_type=3  ! fixed viscosity coefficient set by user
+   mu_hyper=1    ! laplacian^8   grad^16
+   mu_hyper_value = rvalue
 else if (sdata=='hyper16_fix') then
    hyper_type=3  ! fixed viscosity coefficient set by user
    mu_hyper=8    ! laplacian^8   grad^16
