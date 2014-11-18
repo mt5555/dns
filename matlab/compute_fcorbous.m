74ad965884b54a0a00555160783a5e5c72dfa51a
skurien <skurien@users.noreply.github.com>
2014-11-18 13:37:43 -0700

    modified for tall aspect-ratio + clarifying comments

--- matlab/compute_fcorbous.m
+++ matlab/compute_fcorbous.m
@@ -4,10 +4,20 @@ Ro=input('Ro: ');
 epsf = input('energy input rate eps_f: ');
 k_f = input('forcing shell number k_f: ');
 
-k_f = k_f*2*pi/Lz;
-U = (epsf/k_f)^(1/3);
-H = 1/k_f;
-L = H/Lz;
+Lz_short = min(Lz,1)
+
+k_w = k_f*2*pi/Lz_short;
+U = (epsf/k_w)^(1/3);
+
+if Lz <= 1
+  H = 2*pi/k_w; %used to be 1/k_w for the older flows
+  L = H/Lz;
+end
+
+if Lz > 1
+  L = 2*pi/k_w
+  H = L*Lz 
+end
 
 bous = U/H/Fr;
 fcor = U/L/Ro;
